0034cb75675a54788be15298d84934a668dd94eb
蔡诗珺 <1990908685@qq.com>
2018-08-10 00:44:17 +0800

    增加 40 6.11-1 Zuul过滤器

---
+++ microservice-spring-cloud/microservice-gateway-zuul-filter/src/main/java/com/itmuch/cloud/PreZuulFilter.java
@@ -0,0 +1,36 @@
+package com.itmuch.cloud;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PreZuulFilter extends ZuulFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PreZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        String host = request.getRemoteHost();
+        PreZuulFilter.LOGGER.info("请求的host：{}",host);
+        return null;
+    }
+}
