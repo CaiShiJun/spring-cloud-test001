8b6d35e5dfad582779e70d639c95133481c520b6
蔡诗珺 <1990908685@qq.com>
2018-08-17 01:39:17 +0800

    增加 48 7.7 Spring Cloud Config与Eureka

---
+++ Spring-Cloud-Camden/microservice-spring-cloud-practice/microservice-config-client-authc/src/main/java/com/itmuch/cloud/ConfigClientAuthcApplication.java
@@ -0,0 +1,11 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAuthcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientAuthcApplication.class, args);
+    }
+}
